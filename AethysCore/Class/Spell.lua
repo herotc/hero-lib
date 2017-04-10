ee04ab16b3b5f43c10750a9552acf95a53045853
Aethys256 <aethys256@users.noreply.github.com>
2017-04-10 16:32:09 +0200

    [Spell] Cooldown functions refactor

--- AethysCore/Class/Spell.lua
+++ AethysCore/Class/Spell.lua
@@ -19,10 +19,6 @@
   local unpack = unpack;
   local wipe = table.wipe;
   -- File Locals
-  local _T = {                  -- Temporary Vars
-    Charges, MaxCharges,          -- Cooldown / Recharge
-    CDTime, CDValue, CD           -- Cooldown / Recharge
-  };
 
 
 --- ============================ CONTENT ============================
@@ -268,11 +264,35 @@
       return unpack(Cache.SpellInfo[self.SpellID].CooldownInfo);
     end
 
+    -- Computes any spell cooldown.
+    function Spell:ComputeCooldown (BypassRecovery, Type)
+      local Charges, MaxCharges, CDTime, CDValue;
+      if Type == "Charges" then
+        -- Get spell recharge infos
+        Charges, MaxCharges, CDTime, CDValue = self:ChargesInfo();
+        -- Return 0 if the spell has already all its charges.
+        if Charges == MaxCharges then return 0; end
+      else
+        -- Get spell cooldown infos
+        CDTime, CDValue = self:CooldownInfo();
+        -- Return 0 if the spell isn't in CD.
+        if CDTime == 0 then return 0; end
+      end
+      -- Compute the CD.
+      local CD = CDTime + CDValue - AC.GetTime() - (BypassRecovery and 0 or AC.RecoveryOffset());
+      -- Return the Spell CD
+      return CD > 0 and CD or 0;
+    end
+    function Spell:ComputeChargesCooldown (BypassRecovery)
+      return self:ComputeCooldown(BypassRecovery, "Charges");
+    end
+
     -- Get the CostInfo (from GetSpellPowerCost) and cache it.
     function Spell:CostInfo (Key)
       if not Key or type(Key) ~= "string" then error("Invalid Key."); end
       if not Cache.SpellInfo[self.SpellID] then Cache.SpellInfo[self.SpellID] = {}; end
       if not Cache.SpellInfo[self.SpellID].CostInfo then
+        -- hasRequiredAura, type, name, cost, minCost, requiredAuraID, costPercent, costPerSec
         Cache.SpellInfo[self.SpellID].CostInfo = GetSpellPowerCost(self.SpellID)[1];
       end
       return Cache.SpellInfo[self.SpellID].CostInfo[Key];
@@ -338,34 +358,38 @@
     end
 
     -- action.foo.recharge_time or cooldown.foo.recharge_time
-    function Spell:Recharge ()
+    function Spell:Recharge (BypassRecovery)
       if not Cache.SpellInfo[self.SpellID] then Cache.SpellInfo[self.SpellID] = {}; end
-      if not Cache.SpellInfo[self.SpellID].Recharge then
-        -- Get Spell Recharge Infos
-        _T.Charges, _T.MaxCharges, _T.CDTime, _T.CDValue = self:ChargesInfo();
-        -- Return 0 if the Spell isn't in CD.
-        if _T.Charges == _T.MaxCharges then
-          return 0;
+      if (not BypassRecovery and not Cache.SpellInfo[self.SpellID].Recharge)
+        or (BypassRecovery and not Cache.SpellInfo[self.SpellID].RechargeNoRecovery) then
+        if BypassRecovery then
+          Cache.SpellInfo[self.SpellID].RechargeNoRecovery = self:ComputeChargesCooldown(BypassRecovery);
+        else
+          Cache.SpellInfo[self.SpellID].Recharge = self:ComputeChargesCooldown();
         end
-        -- Compute the CD.
-        _T.CD = _T.CDTime + _T.CDValue - AC.GetTime() - AC.RecoveryOffset();
-        -- Return the Spell CD
-        Cache.SpellInfo[self.SpellID].Recharge = _T.CD > 0 and _T.CD or 0;
       end
       return Cache.SpellInfo[self.SpellID].Recharge;
     end
 
     -- action.foo.charges_fractional or cooldown.foo.charges_fractional
     -- TODO : Changes function to avoid using the cache directly
-    function Spell:ChargesFractional ()
+    function Spell:ChargesFractional (BypassRecovery)
       if not Cache.SpellInfo[self.SpellID] then Cache.SpellInfo[self.SpellID] = {}; end
-      if not Cache.SpellInfo[self.SpellID].ChargesFractional then
-        self:Charges(); -- Cache the charges infos to use the cache directly after. 
+      if (not BypassRecovery and not Cache.SpellInfo[self.SpellID].ChargesFractional)
+        or (BypassRecovery and not Cache.SpellInfo[self.SpellID].ChargesFractionalNoRecovery) then
         if self:Charges() == self:MaxCharges() then
-          Cache.SpellInfo[self.SpellID].ChargesFractional = self:Charges();
+          if BypassRecovery then
+            Cache.SpellInfo[self.SpellID].ChargesFractionalNoRecovery = self:Charges();
+          else
+            Cache.SpellInfo[self.SpellID].ChargesFractional = self:Charges();
+          end
         else
           -- charges + (chargeDuration - recharge) / chargeDuration
-          Cache.SpellInfo[self.SpellID].ChargesFractional = self:Charges() + (self:ChargesInfo(4)-self:Recharge())/self:ChargesInfo(4);
+          if BypassRecovery then
+            Cache.SpellInfo[self.SpellID].ChargesFractionalNoRecovery = self:Charges() + (self:ChargesInfo(4)-self:Recharge(BypassRecovery))/self:ChargesInfo(4);
+          else
+            Cache.SpellInfo[self.SpellID].ChargesFractional = self:Charges() + (self:ChargesInfo(4)-self:Recharge())/self:ChargesInfo(4);
+          end
         end
       end
       return Cache.SpellInfo[self.SpellID].ChargesFractional;
@@ -379,21 +403,12 @@
     -- cooldown.foo.remains
     function Spell:CooldownRemains (BypassRecovery)
       if not Cache.SpellInfo[self.SpellID] then Cache.SpellInfo[self.SpellID] = {}; end
-      if (not BypassRecovery and not Cache.SpellInfo[self.SpellID].Cooldown) or (BypassRecovery and not Cache.SpellInfo[self.SpellID].CooldownNoRecovery) then
-        -- Get Spell Cooldown Infos
-        _T.CDTime, _T.CDValue = self:CooldownInfo();
-        -- Return 0 if the Spell isn't in CD.
-        if _T.CDTime == 0 then
-          return 0;
-        end
-        -- Compute the CD.
-        _T.CD = _T.CDTime + _T.CDValue - AC.GetTime() - (BypassRecovery and 0 or AC.RecoveryOffset());
+      if (not BypassRecovery and not Cache.SpellInfo[self.SpellID].Cooldown)
+        or (BypassRecovery and not Cache.SpellInfo[self.SpellID].CooldownNoRecovery) then
         if BypassRecovery then
-          -- Return the Spell CD
-          Cache.SpellInfo[self.SpellID].CooldownNoRecovery = _T.CD > 0 and _T.CD or 0;
+          Cache.SpellInfo[self.SpellID].CooldownNoRecovery = self:ComputeCooldown(BypassRecovery);
         else
-          -- Return the Spell CD
-          Cache.SpellInfo[self.SpellID].Cooldown = _T.CD > 0 and _T.CD or 0;
+          Cache.SpellInfo[self.SpellID].Cooldown = self:ComputeCooldown();
         end
       end
       return BypassRecovery and Cache.SpellInfo[self.SpellID].CooldownNoRecovery or Cache.SpellInfo[self.SpellID].Cooldown;
