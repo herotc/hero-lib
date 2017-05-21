605e2ea82f1037d6fbe2dd76c9cf7d241d5f5e3c
Aethys256 <aethys256@users.noreply.github.com>
2017-05-21 16:52:20 +0200

    [Prev_Spell] Pet + Whitelist + Blacklist

--- AethysCore/Events/Prev_Spell.lua
+++ AethysCore/Events/Prev_Spell.lua
@@ -6,41 +6,68 @@
   local Cache = AethysCache;
   local Unit = AC.Unit;
   local Player = Unit.Player;
+  local Pet = Unit.Pet;
   local Target = Unit.Target;
   local Spell = AC.Spell;
   local Item = AC.Item;
   -- Lua
+  local pairs = pairs;
   local tableinsert = table.insert;
   -- File Locals
   local TriggerGCD = AC.Enum.TriggerGCD; -- TriggerGCD table until it has been filtered.
   local LastRecord = 15;
-  local PrevGCD, PrevOffGCD = {}, {};
+  local Prev = {
+    GCD = {},
+    OffGCD = {},
+    PetGCD = {},
+    PetOffGCD = {}
+  };
+  local Custom = {
+    Whitelist = {},
+    Blacklist = {}
+  };
 
 
 --- ============================ CONTENT ============================
   
   -- Init all the records at 0, so it saves one check on PrevGCD method.
   for i = 1, LastRecord do
-    tableinsert(PrevGCD, 0);
-    tableinsert(PrevOffGCD, 0);
+    for _, Table in pairs(Prev) do
+      tableinsert(Table, 0);
+    end
   end
 
   -- Clear Old Records
   local function RemoveOldRecords ()
-    local n = #PrevGCD;
-    while n > LastRecord do
-      PrevGCD[n] = nil;
-      n = n - 1;
+    for _, Table in pairs(Prev) do
+      local n = #Table;
+      while n > LastRecord do
+        Table[n] = nil;
+        n = n - 1;
+      end
     end
   end
 
-  -- On Cast Success Listener
+  -- Player On Cast Success Listener
   AC:RegisterForSelfCombatEvent(
     function (_, _, _, _, _, _, _, _, _, _, _, SpellID)
       if TriggerGCD[SpellID] then
-        tableinsert(PrevGCD, 1, SpellID);
+        tableinsert(Prev.GCD, 1, SpellID);
       elseif TriggerGCD[SpellID] == false then -- Prevents unwanted spells to be registered as OffGCD.
-        tableinsert(PrevOffGCD, 1, SpellID);
+        tableinsert(Prev.OffGCD, 1, SpellID);
+      end
+      RemoveOldRecords();
+    end
+    , "SPELL_CAST_SUCCESS"
+  )
+
+  -- Pet On Cast Success Listener
+  AC:RegisterForPetCombatEvent(
+    function (_, _, _, _, _, _, _, _, _, _, _, SpellID)
+      if TriggerGCD[SpellID] then
+        tableinsert(Prev.PetGCD, 1, SpellID);
+      elseif TriggerGCD[SpellID] == false then -- Prevents unwanted spells to be registered as OffGCD.
+        tableinsert(Prev.PetOffGCD, 1, SpellID);
       end
       RemoveOldRecords();
     end
@@ -48,26 +75,64 @@
   )
 
   -- Filter the Enum TriggerGCD table to keep only registered spells for a given class (based on SpecID).
-  function Unit:FilterTriggerGCD (SpecID)
+  function Player:FilterTriggerGCD (SpecID)
     local RegisteredSpells = {};
     local BaseTriggerGCD = AC.Enum.TriggerGCD; -- In case FilterTriggerGCD is called multiple time, we take the Enum table as base.
+    -- Fetch registered spells during the init
     for Spec, Spells in pairs(AC.Spell[AC.SpecID_ClassesSpecs[SpecID][1]]) do
       for _, Spell in pairs(Spells) do
         local SpellID = Spell:ID();
-        RegisteredSpells[SpellID] = BaseTriggerGCD[SpellID];
+        local TriggerGCDInfo = BaseTriggerGCD[SpellID];
+        if TriggerGCDInfo ~= nil then
+          RegisteredSpells[SpellID] = TriggerGCDInfo;
+        end
+      end
+    end
+    -- Add Spells based on the Whitelist
+    for SpellID, Value in pairs(Custom.Whitelist) do
+      RegisteredSpells[SpellID] = Value;
+    end
+    -- Remove Spells based on the Blacklist
+    for i = 1, #Custom.Blacklist do
+      local SpellID = Custom.Blacklist[i];
+      if RegisteredSpells[SpellID] then
+        RegisteredSpells[SpellID] = nil;
       end
     end
     TriggerGCD = RegisteredSpells;
   end
 
+  -- Add spells in the Trigger GCD Whitelist
+  function Spell:AddToTriggerGCD (Value)
+    if type(Value) ~= "boolean" then error("You must gives a boolean as argument."); end
+    Custom.Whitelist[self.SpellID] = Value;
+  end
+
+  -- Add spells in the Trigger GCD Blacklist
+  function Spell:RemoveFromTriggerGCD ()
+    tableinsert(Custom.Blacklist, self.SpellID);
+  end
+
   -- prev_gcd.x.foo
-  function Unit:PrevGCD (Index, Spell)
+  function Player:PrevGCD (Index, Spell)
     if Index > LastRecord then error("Only the lasts " .. LastRecord  .. " GCDs can be checked."); end
-    return PrevGCD[Index] == Spell:ID();
+    return Prev.GCD[Index] == Spell:ID();
   end
 
   -- prev_off_gcd.x.foo
-  function Unit:PrevOffGCD (Index, Spell)
+  function Player:PrevOffGCD (Index, Spell)
     if Index > LastRecord then error("Only the lasts " .. LastRecord  .. " OffGCDs can be checked."); end
-    return PrevOffGCD[Index] == Spell:ID();
+    return Prev.OffGCD[Index] == Spell:ID();
+  end
+
+  -- "pet.prev_gcd.x.foo"
+  function Pet:PrevGCD (Index, Spell)
+    if Index > LastRecord then error("Only the lasts " .. LastRecord  .. " GCDs can be checked."); end
+    return Prev.PetGCD[Index] == Spell:ID();
+  end
+
+  -- "pet.prev_off_gcd.x.foo"
+  function Pet:PrevOffGCD (Index, Spell)
+    if Index > LastRecord then error("Only the lasts " .. LastRecord  .. " OffGCDs can be checked."); end
+    return Prev.PetOffGCD[Index] == Spell:ID();
   end
