4fd1ac9de42d0411f3628fafafeb79dd8e32e135
ghr74 <28192634+ghr74@users.noreply.github.com>
2018-02-24 02:45:50 +0100

    [Events] Change KeyBinding detection system

--- AethysCore/Events/KeyBinding.lua
+++ AethysCore/Events/KeyBinding.lua
@@ -16,47 +16,33 @@
   local tableinsert = table.insert;
   -- File Locals
   local KeyBindings = {};
-
+  local BarNames = {};
 
 --- ============================ CONTENT ============================
-  -- Helper to reduce the Keybinds
-  local ShortenKB;
-  do
-    local ShortKBSubString = {
-      ["-"] = ":", -- TODO: Add options to choose the separator : { ":", "", "~", ",", ".", "_", "-", " "}.
-      ["ALT"] = "A",
-      ["CTRL"] = "C",
-      ["SHIFT"] = "S",
-      ["NUMPAD"] = "N",
-      ["DIVIDE"] = "/",
-      ["MINUS"] = "-",
-      ["MULTIPLY"] = "*",
-      ["PLUS"] = "+",
-      ["BUTTON"] = "M"
-    };
-    function ShortenKB (KeyBinding)
-      for Pattern, Replace in pairs ( ShortKBSubString ) do
-        KeyBinding = stringgsub( KeyBinding, Pattern, Replace );
+  -- Parse a given ActionBar for HotKeys
+  local function ParseBar ( Bar, Override )
+    local Button;
+    local ButtonTexture;
+    local ButtonHotKey = "";
+    for i = 1, Bar[2] do
+      Button = _G[Bar[1] .. i];
+      if Button and Button.icon and Button.HotKey then
+        ButtonTexture = Button.icon:GetTexture();
+        ButtonHotKey = Button.HotKey:GetText();
+        if Button.icon:IsShown() and ButtonTexture and ButtonHotKey and strbyte(ButtonHotKey) ~= 226 then
+          if not KeyBindings[ButtonTexture] or Override then
+            KeyBindings[ButtonTexture] = ButtonHotKey;
+          end
+        end
       end
-
-      -- Hotfix for Numpad "-"
-      KeyBinding = stringgsub( KeyBinding, "N:", "N-" );
-
-      return KeyBinding;
     end
   end
 
   --[[*
     * @function FindKeyBindings
-    * @desc List every actions presents in action bars, sorted by their type (item, macro, spell).
-    *
-    * @returns {table}
+    * @desc Populate KeyBindings table with <TextureID> = <HotKey> data.
     *]]
-  -- Note: There is currently one issue, if the player has a different KeyBind for the same action
-  --   i.e. the same ActionID, then it will return the KeyBind of the first occurence.
-  --   One way to fix that would be to add the ShapeshiftIndex (see: http://wowwiki.wikia.com/wiki/API_GetShapeshiftForm)
-  --   Since it is unlikely to happen (it's kinda counter-intuitive), I won't bother implementing that.
-  --   Feel free to PR/Fix it if you want.
+
   local function FindKeyBindings ()
     -- SlotIndex      ActionFrame                     CommandName                 Page
     -- 1..12        = ActionButton (Primary Bar)      ACTIONBUTTON..Slot          1
@@ -70,102 +56,57 @@
     -- and j is the bar index in 1..12 for MULTIACTIONBARs
     -- See: http://wowwiki.wikia.com/wiki/ActionSlot
     -- BT stands for Bartender, it needs a special handling.
-    local Commands = {};
 
-    --- Populate Actions
-    -- Default UI
-    local CommandNames = {
-      [3] = "MULTIACTIONBAR3BUTTON",
-      [4] = "MULTIACTIONBAR4BUTTON",
-      [5] = "MULTIACTIONBAR2BUTTON",
-      [6] = "MULTIACTIONBAR1BUTTON"
-    };
-    -- Bartender
-    local BTCommandLeft = "CLICK BT4Button";
-    local BTCommandRight = ":LeftButton";
-    -- Iterate over the Slots
-    for i = 1, 11 do
-      local CommandName = CommandNames[ i ] or "ACTIONBUTTON";
-      for j = 1, 12 do
-        local Slot = 12 * (i - 1) + j;
-        -- Default UI
-        local CommandName;
-        if CommandNames[ i ] then
-          CommandName = CommandNames[ i ] .. j;
-        else
-          CommandName = "ACTIONBUTTON" .. j;
-        end
-        if HasAction( Slot ) then
-          local ActionType, ActionID, ActionSubtype = GetActionInfo( Slot );
-          if not Commands[ CommandName ] then Commands[ CommandName ] = {}; end
-          tableinsert( Commands[ CommandName ], {
-            ActionType = ActionType,
-            ActionID = ActionID,
-            ActionSubtype = ActionSubtype
-          } );
-        end
+    --- Populate Bar Names
+    do
+      if _G.Bartender4 then
         -- Bartender
-        -- TODO: Avoid code duplication
-        local BTCommandName = BTCommandLeft .. Slot .. BTCommandRight;
-        local BTKeybind = GetBindingKey(BTCommandName);
-        local Binding = BTKeybind and ShortenKB(BTKeybind) or "";
-        if Binding then
-          local ActionType, ActionID, ActionSubtype = GetActionInfo( Slot );
-          if not Commands[ BTCommandName ] then Commands[ BTCommandName ] = {}; end
-          tableinsert( Commands[ BTCommandName ], {
-            ActionType = ActionType,
-            ActionID = ActionID,
-            ActionSubtype = ActionSubtype,
-            Binding = Binding
-          } );
-        end
+        BarNames = {
+          [1] = {"BT4Button", 120},
+        }
+      elseif _G.ElvUI and _G.ElvUI[1].ActionBars then
+        -- ElvUI
+        BarNames = {
+          [1] = {"ElvUI_Bar1Button", _G.ElvUI[1].ActionBars.db.bar1.buttons},
+          [2] = {"ElvUI_Bar2Button", _G.ElvUI[1].ActionBars.db.bar2.buttons},
+          [3] = {"ElvUI_Bar3Button", _G.ElvUI[1].ActionBars.db.bar3.buttons},
+          [4] = {"ElvUI_Bar4Button", _G.ElvUI[1].ActionBars.db.bar4.buttons},
+          [5] = {"ElvUI_Bar5Button", _G.ElvUI[1].ActionBars.db.bar5.buttons},
+          [6] = {"ElvUI_Bar6Button", _G.ElvUI[1].ActionBars.db.bar6.buttons},
+        }
+      else
+        -- Default UI
+        BarNames = {
+          [1] = {"ActionButton", 12},
+          [2] = {"ActionButton", 12},
+          [3] = {"MultiBarRightButton", 12},
+          [4] = {"MultiBarLeftButton", 12},
+          [5] = {"MultiBarBottomRightButton", 12},
+          [6] = {"MultiBarBottomLeftButton", 12},
+        };
       end
     end
-
-    --- Populate KeyBindings
-    for i = 1, GetNumBindings() do
-      local CommandName, Category, Binding1, Binding2 = GetBinding( i );
-      local Actions = Commands[ CommandName ];
-      if Actions then
-        local Binding = Binding1 and ShortenKB( Binding1 ) or "";
-        for _, Action in pairs( Actions ) do
-          Action.Binding = Binding;
-        end
-      end
+    --- Parse Bars
+    for i = 1, #BarNames do
+      ParseBar(BarNames[i], true);
     end
 
-    --- Map Actions -> KeyBindings
-    local KeyBindings = {
-      Item = {},
-      Macro = {},
-      Spell = {}
-    };
-    for _, Actions in pairs( Commands ) do
-      for _, Action in pairs( Actions ) do
-        local ActionType = Action.ActionType;
-        local ActionID = Action.ActionID;
-        local Binding = Action.Binding;
-        if Binding and Binding ~= "" then
-          if ActionType == "item" and not KeyBindings.Item[ ActionID ] then
-            KeyBindings.Item[ ActionID ] = Binding;
-          elseif ActionType == "macro" then
-            local Name = GetMacroInfo( ActionID );
-            if Name and not KeyBindings.Macro[ Name ] then
-              KeyBindings.Macro[ Name ] = Binding;
-            end
-          elseif ActionType == "spell" and not KeyBindings.Spell[ ActionID ] then
-            KeyBindings.Spell[ ActionID ] = Binding;
-          end
-        end
-      end
-    end
-
-    return KeyBindings;
   end
 
   AC:RegisterForEvent(
     function ()
-      KeyBindings = FindKeyBindings();
+      C_Timer.After(0.001, 
+        function() 
+          FindKeyBindings(); 
+        end
+      ); -- on a timer, because of Bar Update Delay
+    end
+    , "UPDATE_SHAPESHIFT_FORM"  
+  );
+  
+  AC:RegisterForEvent(
+    function ()
+      FindKeyBindings();
     end
     , "ZONE_CHANGED_NEW_AREA"
     , "PLAYER_SPECIALIZATION_CHANGED"
@@ -175,15 +116,6 @@
     , "LEARNED_SPELL_IN_TAB"
   );
 
-  --- TODO: With the Action class (Item/Macro/Spell) rework, this is going to change.
-  function AC.FindMacroKeyBinding (Name)
-    return KeyBindings.Macro[ Name ] or false;
-  end
-
-  function Item:FindKeyBinding ()
-    return KeyBindings.Item[ self.ItemID ] or false;
-  end
-
-  function Spell:FindKeyBinding ()
-    return KeyBindings.Spell[ self.SpellID ] or false;
+  function AC.FindKeyBinding (TextureID)
+    return KeyBindings[TextureID] or false;
   end
