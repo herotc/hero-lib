c2861011d00a4812da62479c62c2cbd744a3cc61
Julien Rechaussat <julien.j.rechaussat@gmail.com>
2019-04-18 11:32:04 +0200

    [Enum] 8.1.5.29981 data

--- HeroLib/Enum/ItemRange.lua
+++ HeroLib/Enum/ItemRange.lua
@@ -1,4 +1,4 @@
--- Generated using WoW 8.0.1.27219 client data on 2018-08-03T04:23:30.494798.
+-- Generated using WoW 8.1.5.29981 client data on 2019-04-18T10:33:03.890585.
 HeroLib.Enum.ItemRange = {
   Hostile = {
     RangeIndex = {"Melee", 5, 7, 8, 10, 15, 20, 25, 30, 35, 38, 40, 45, 50, 55, 60, 70, 80, 90, 100},
