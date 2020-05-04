34f27a0ab381c87aa751b6a5ef6c7898d6dfde24
EvanMichaels <evan.michaels@gmail.com>
2020-05-04 02:45:11 -0400

    [Prev_Spell] Add/Adjust Helper Functions for Predicted Casts
    * Since instant attacks don't trigger SPELL_CAST_START, add some functions to track the last known suggestion prior to the start of the current GCD
    * If we have a tracked value and the current GCD is in progress, but a SPELL_CAST_SUCCESS has not yet triggered, we can use this suggested value temporarily until we get the event
    * Additionally, add some helper functions for fetching the last known GCD and off-GCD cast time

--- HeroLib/Class/Unit/Player/Stat.lua
+++ HeroLib/Class/Unit/Player/Stat.lua
@@ -51,6 +51,14 @@ do
   function Player:GCDRemains()
     return GCDSpell:CooldownRemains(true)
   end
+  
+  function Player:GCDStartTime()
+    local GCDStartTime, GCDDuration = GCDSpell:CooldownInfo()
+    if GCDDuration > 0 then
+      return GCDStartTime
+    end
+    return 0
+  end
 end
 
 -- attack_power
--- HeroLib/Events/Prev_Spell.lua
+++ HeroLib/Events/Prev_Spell.lua
@@ -13,10 +13,13 @@ local Item = HL.Item
 -- Lua
 local pairs = pairs
 local tableinsert = table.insert
+local mathmax = math.max
 -- File Locals
 local TriggerGCD = HL.Enum.TriggerGCD -- TriggerGCD table until it has been filtered.
 local LastRecord = 15
 local PrevGCDPredicted = 0
+local PrevGCDCastTime = 0
+local PrevOffGCDCastTime = 0
 local Prev = {
   GCD = {},
   OffGCD = {},
@@ -27,7 +30,11 @@ local Custom = {
   Whitelist = {},
   Blacklist = {}
 }
-
+local PrevSuggested = {
+  Spell = nil,
+  Time = 0
+}
+local GCDSpell = Spell(61304)
 
 --- ============================ CONTENT ============================
 
@@ -51,17 +58,24 @@ end
 
 -- Player On Cast Success Listener
 HL:RegisterForSelfCombatEvent(function(_, _, _, _, _, _, _, _, _, _, _, SpellID)
+  HL.Timer.LastCastSuccess = HL.GetTime()
   if TriggerGCD[SpellID] ~= nil then
     if TriggerGCD[SpellID] then
+      -- HL.Print(HL.GetTime() .. " Self SPELL_CAST_SUCCESS " .. SpellID)
       tableinsert(Prev.GCD, 1, SpellID)
+      PrevGCDCastTime = HL.GetTime()
       Prev.OffGCD = {}
+      PrevOffGCDCastTime = 0
       PrevGCDPredicted = 0
     else -- Prevents unwanted spells to be registered as OffGCD.
       tableinsert(Prev.OffGCD, 1, SpellID)
+      PrevOffGCDCastTime = HL.GetTime()
+      PrevGCDCastTime = 0
     end
   end
   RemoveOldRecords()
 end, "SPELL_CAST_SUCCESS")
+
 HL:RegisterForSelfCombatEvent(function(_, _, _, _, _, _, _, _, _, _, _, SpellID)
   if TriggerGCD[SpellID] then
     PrevGCDPredicted = SpellID
@@ -72,6 +86,7 @@ HL:RegisterForSelfCombatEvent(function(_, _, _, _, _, _, _, _, _, _, _, SpellID)
     PrevGCDPredicted = 0
   end
 end, "SPELL_CAST_FAILED")
+
 -- Pet On Cast Success Listener
 HL:RegisterForPetCombatEvent(function(_, _, _, _, _, _, _, _, _, _, _, SpellID)
   if TriggerGCD[SpellID] ~= nil then
@@ -124,6 +139,50 @@ function Spell:RemoveFromTriggerGCD()
   tableinsert(Custom.Blacklist, self.SpellID)
 end
 
+-- Time of the last on-GCD SPELL_CAST_SUCCESS
+function Player:PrevGCDTime()
+  return PrevGCDCastTime
+end
+
+-- Time of the last off-GCD SPELL_CAST_SUCCESS
+function Player:PrevOffGCDTime()
+  return PrevOffGCDCastTime
+end
+
+-- Time of the last SPELL_CAST_SUCCESS of any type
+function Player:PrevCastTime()
+  return mathmax(PrevGCDCastTime, PrevOffGCDCastTime)
+end
+
+-- Returns if a GCD has been started but we don't yet know what the spell is
+function Player:IsPrevCastPending()
+  -- If we recieved a SPELL_CAST_START event, we know about the cast
+  if PrevGCDPredicted > 0 then
+    return false
+  end
+
+  -- Otherwise, check to see if the GCD was started after the last known SPELL_CAST_SUCCESS
+  local GCDStartTime, GCDDuration = GCDSpell:CooldownInfo()
+  if GCDDuration > 0 and GCDStartTime > PrevGCDCastTime then
+    return true
+  end
+
+  return false
+end
+
+-- Sets the last known tracked suggestion before the start of the next GCD
+function Player:SetPrevSuggestedSpell(SuggestedSpell)
+  if SuggestedSpell == nil or SuggestedSpell.SpellID ~= nil then
+    -- Don't update the previous suggested spell if we are currently on the GCD
+    local GCDStartTime, GCDDuration = GCDSpell:CooldownInfo()
+    if GCDDuration > 0 and GCDStartTime > PrevGCDCastTime then
+      return
+    end
+    PrevSuggested.Spell = SuggestedSpell
+    PrevSuggested.Time = HL.GetTime()
+  end
+end
+
 -- prev_gcd.x.foo
 function Player:PrevGCD(Index, Spell)
   if Index > LastRecord then error("Only the last " .. LastRecord .. " GCDs can be checked.") end
@@ -137,9 +196,21 @@ end
 -- Player:PrevGCD with cast start prediction
 function Player:PrevGCDP(Index, Spell, ForcePred)
   if Index > LastRecord then error("Only the last " .. (LastRecord) .. " GCDs can be checked.") end
-  if PrevGCDPredicted > 0 and Index == 1 or ForcePred then
-    return PrevGCDPredicted == Spell:ID()
-  elseif PrevGCDPredicted > 0 then
+
+  -- If we don't have a PrevGCDPredicted from SPELL_CAST_START, attempt to use the last suggested spell instead
+  -- This is only used when the local GCD has begun but a SPELL_CAST_SUCCESS has not yet fired to determine what the spell is
+  local PredictedGCD = PrevGCDPredicted
+  if PredictedGCD == 0 and PrevSuggested.Spell and PrevSuggested.Time > PrevGCDCastTime then
+    local SpellId = PrevSuggested.Spell:ID()
+    local GCDStartTime, GCDDuration = GCDSpell:CooldownInfo()
+    if GCDDuration > 0 and GCDStartTime > PrevGCDCastTime and TriggerGCD[SpellId] then
+      PredictedGCD = SpellId
+    end
+  end
+
+  if PredictedGCD > 0 and Index == 1 or ForcePred then
+    return PredictedGCD == Spell:ID()
+  elseif PredictedGCD > 0 then
     return Player:PrevGCD(Index - 1, Spell)
   else
     return Player:PrevGCD(Index, Spell)
