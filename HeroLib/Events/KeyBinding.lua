b24124107998439119875d94d8cf3df61f9c0b70
Quentin Giraud <dev@aethys.io>
2020-07-23 19:55:48 +0200

    Compatibility fixes with 9.0

--- HeroLib/Events/KeyBinding.lua
+++ HeroLib/Events/KeyBinding.lua
@@ -33,7 +33,7 @@ local function ParseBar(Bar, Override)
       end
       if Button.icon:IsShown() and ButtonTexture and ButtonHotKey and strbyte(ButtonHotKey) ~= 226 then
         --If button is a macro check that the macro casts a spell else ignore
-        local buttonActionType, buttonActionId = GetActionInfo(ActionButton_GetPagedID(Button))
+        local buttonActionType, buttonActionId = GetActionInfo(Button:GetPagedID())
         if buttonActionType == "macro" then
           --Item is a macro so check it plans to cast a spell
           local macrospellid = GetMacroSpell(buttonActionId)
--- HeroLib/Lib/LibUIDropDownMenu/LibUIDropDownMenu.lua
+++ HeroLib/Lib/LibUIDropDownMenu/LibUIDropDownMenu.lua
@@ -218,7 +218,7 @@ local function creatre_UIDropDownList(name, parent)
 	f:SetFrameStrata("DIALOG")
 	f:EnableMouse(true)
 
-	f.Backdrop = _G[name.."Backdrop"] or CreateFrame("Frame", name.."Backdrop", f)
+	f.Backdrop = _G[name.."Backdrop"] or CreateFrame("Frame", name.."Backdrop", f, "BackdropTemplate")
 	f.Backdrop:SetAllPoints()
 	f.Backdrop:SetBackdrop({
 		bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background-Dark",
@@ -229,7 +229,7 @@ local function creatre_UIDropDownList(name, parent)
 		insets = { left = 11, right = 12, top = 12, bottom = 9, },
 	})
 
-	f.MenuBackdrop= _G[name.."MenuBackdrop"] or CreateFrame("Frame", name.."MenuBackdrop", f)
+	f.MenuBackdrop= _G[name.."MenuBackdrop"] or CreateFrame("Frame", name.."MenuBackdrop", f, "BackdropTemplate")
 	f.MenuBackdrop:SetAllPoints()
 	f.MenuBackdrop:SetBackdrop({
 		bgFile = "Interface\\Tooltips\\UI-Tooltip-Background",
