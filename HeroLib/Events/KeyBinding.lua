33a1d01edc881110fff4f4c1b9a2334b0813905f
Cilraaz <cilraaz@gmail.com>
2020-08-19 09:24:24 -0400

    [KeyBinding] Fix Error with ElvUI

--- HeroLib/Events/KeyBinding.lua
+++ HeroLib/Events/KeyBinding.lua
@@ -33,7 +33,11 @@ local function ParseBar(Bar, Override)
       end
       if Button.icon:IsShown() and ButtonTexture and ButtonHotKey and strbyte(ButtonHotKey) ~= 226 then
         --If button is a macro check that the macro casts a spell else ignore
-        local buttonActionType, buttonActionId = GetActionInfo(Button:GetPagedID())
+        if _G.ElvUI then
+          local buttonActionType, buttonActionId = GetActionInfo(Button._state_action)
+        else
+          local buttonActionType, buttonActionId = GetActionInfo(Button:GetPagedID())
+        end
         if buttonActionType == "macro" then
           --Item is a macro so check it plans to cast a spell
           local macrospellid = GetMacroSpell(buttonActionId)
