e7c8f9281752df81daebe805aaee6ff615477578
Quentin Giraud <dev@aethys.io>
2020-09-21 01:31:55 +0200

    fix: [Overrides]  Add missing units

--- HeroLib/Misc/Overrides.lua
+++ HeroLib/Misc/Overrides.lua
@@ -3,10 +3,12 @@
 -- Addon
 local addonName, HL = ...
 -- HeroLib
-local Cache = HeroCache
-local Unit = HL.Unit
-local Player = Unit.Player
-local Target = Unit.Target
+local Cache, Utils = HeroCache, HL.Utils
+local Unit, UnitGUIDMap = HL.Unit, HL.UnitGUIDMap
+local Player, Pet, Target = Unit.Player, Unit.Pet, Unit.Target
+local Focus, MouseOver = Unit.Focus, Unit.MouseOver
+local Arena, Boss, Nameplate = Unit.Arena, Unit.Boss, Unit.Nameplate
+local Party, Raid = Unit.Party, Unit.Raid
 local Spell = HL.Spell
 local Item = HL.Item
 -- Lua
