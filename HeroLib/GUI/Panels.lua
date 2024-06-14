1eea88912037b34feb1e04be0b75ffb4aea97f3b
Cilraaz <cilraaz@gmail.com>
2024-06-14 17:12:16 -0400

    feat(GUI): Spacing Changes

--- HeroLib/GUI/Panels.lua
+++ HeroLib/GUI/Panels.lua
@@ -1,15 +1,17 @@
 --- ============================ HEADER ============================
 --- ======= LOCALIZE =======
 -- Addon
-local addonName, HL = ...
+local addonName, HL  = ...
 -- HeroLib
-local Utils = HL.Utils
--- Lua
-local stringformat = string.format
-local strsplit = strsplit
+local Utils          = HL.Utils
+
+-- Lua locals
+local stringformat   = string.format
+local strsplit       = strsplit
+
 -- File Locals
-HL.GUI = {}
-local GUI = HL.GUI
+HL.GUI               = {}
+local GUI            = HL.GUI
 local StringToNumberIfPossible = Utils.StringToNumberIfPossible
 local SubStringCount = Utils.SubStringCount
 
