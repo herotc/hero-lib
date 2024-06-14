06ed310729bd1f2c7cd61de08a6c9d87c8ed2b15
Cilraaz <cilraaz@gmail.com>
2024-06-14 11:12:51 -0400

    feat(Pet): Add Accepts/Returns for API calls; Spacing changes

--- HeroLib/Class/Unit/Pet/Main.lua
+++ HeroLib/Class/Unit/Pet/Main.lua
@@ -1,20 +1,24 @@
 --- ============================ HEADER ============================
 --- ======= LOCALIZE =======
 -- Addon
-local addonName, HL = ...
+local addonName, HL          = ...
 -- HeroLib
-local Cache, Utils = HeroCache, HL.Utils
-local Unit = HL.Unit
-local Player, Pet, Target = Unit.Player, Unit.Pet, Unit.Target
-local Focus, MouseOver = Unit.Focus, Unit.MouseOver
+local Cache, Utils           = HeroCache, HL.Utils
+local Unit                   = HL.Unit
+local Player, Pet, Target    = Unit.Player, Unit.Pet, Unit.Target
+local Focus, MouseOver       = Unit.Focus, Unit.MouseOver
 local Arena, Boss, Nameplate = Unit.Arena, Unit.Boss, Unit.Nameplate
-local Party, Raid = Unit.Party, Unit.Raid
-local Spell = HL.Spell
-local Item = HL.Item
--- Lua
-local IsPetActive = IsPetActive
--- File Locals
+local Party, Raid            = Unit.Party, Unit.Raid
+local Spell                  = HL.Spell
+local Item                   = HL.Item
 
+-- Base API locals
+local IsPetActive            = IsPetActive
+-- Accepts: nil; Returns: petActive (bool)
+
+-- lua locals
+
+-- File Locals
 
 
 --- ============================ CONTENT ============================
