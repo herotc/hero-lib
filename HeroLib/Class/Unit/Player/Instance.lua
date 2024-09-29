ef469dba91e3e57df1761dcc062296cf48aca469
Cilraaz <cilraaz@gmail.com>
2024-09-29 14:45:51 -0400

    feat(Instance): Add Player:CheckDelveQuestProgress()
    - Just a potentially useful function for manual dumping. Not used otherwise. It's too big for a macro.

--- HeroLib/Class/Unit/Player/Instance.lua
+++ HeroLib/Class/Unit/Player/Instance.lua
@@ -16,8 +16,16 @@ local Item                   = HL.Item
 local GetInstanceInfo        = GetInstanceInfo
 -- Accepts: nil; Returns: name (string), instanceType (string), difficulty (number), difficultyName (string), maxPlayers (number),
 -- playerDifficulty (number), isDynamicInstance (bool), instanceID (number), instanceGroupSize (number), lfgDungeonID (number)
+
+-- C_PartyInfo locals
 local IsDelveInProgress      = C_PartyInfo.IsDelveInProgress
 -- Accepts: nil; Returns: isDelveComplete (bool)
+
+-- C_QuestLog locals
+local GetTitleForQuestID     = C_QuestLog.GetTitleForQuestID
+-- Accepts: questID (number); Returns: title (string)
+local IsQuestFlaggedCompleted = C_QuestLog.IsQuestFlaggedCompleted
+-- Accepts: questID (number); Returns: isCompleted (bool)
 
 -- lua locals
 
@@ -64,4 +72,18 @@ end
 -- Get whether the player is in a delve.
 function Player:IsInDelve()
   return IsDelveInProgress()
+end
+
+-- Check Delve quest progress.
+function Player:CheckDelveQuestProgress()
+  local QuestIDs = { 83758, 83759, 85648, 85649, 83766, 85664, 83769, 83767, 83768, 85667, 83771, 83770, 85666 }
+  for _, Quest in ipairs(QuestIDs) do
+    local QuestTitle = GetTitleForQuestID(Quest)
+    if not QuestTitle then
+      QuestTitle = GetTitleForQuestID(Quest)
+    end
+    local QuestComplete = IsQuestFlaggedCompleted(Quest)
+    local CompleteText = QuestComplete and "|cff00ff00Yes|r" or "|cffff0000No|r"
+    HL.Print(QuestTitle.." ("..Quest.."): "..CompleteText)
+  end
 end
